57d704baae2124f24270055c3b302eac852ec7cf
GustavoNakamuraC <nycolysennaa@gmail.com>
2025-03-20 22:20:02 -0300

    JavaScript

---
+++ script.js
@@ -0,0 +1,23 @@
+var primeiroTipo; //variavel global;
+let segundoTipo; //let palavra reservada quando ta dentro de função (for);
+const teceiroTipo = 10; //valor fixo, registrar uma aliquota, pq é fixa (Objeto);
+
+var nome = "Nicoly";
+var idade = 18;
+
+console.log(nome);
+console.log(idade);
+
+console.log ('------------------');
+
+var nome = "Anna";
+var idade = 18;
+
+console.log(nome);
+console.log(idade);
+
+
+function escrever(){ //Função em JavaScript
+    document.getElementById('texto').innerText = 
+    "oi, voce me clicou"; //acessar uma tag pro meio do ID
+}
